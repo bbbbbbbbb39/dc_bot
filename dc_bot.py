1f15f1a4d586c38537b9442b707c2338a17770f6
bbbbbbbbb39 <bohdansokolov@gmail.com>
2026-02-27 21:42:43 +0200

    Update dc_bot.py

--- dc_bot.py
+++ dc_bot.py
@@ -3,26 +3,39 @@ import telebot
 import re
 
 TOKEN = os.getenv("TOKEN")
-CHANNEL_ID = os.getenv("CHANNEL_ID")
 
 bot = telebot.TeleBot(TOKEN)
 
-# Заборонені слова (буде ловити і великі і малі)
+# Канали куди пересилати
+CHANNELS = [
+    -1003733535025,   # перший канал
+    -1003837938772,   # другий канал (встав сюди ID)
+]
+
+# Загальний список заборонених слів
 FORBIDDEN_PATTERNS = [
     r"\bреб_перу\b",
-    r"\bреб\b"
+    r"\bреб\b",
+    r"\blima\b",
+    r"\bperu\b"
 ]
 
 @bot.message_handler(func=lambda message: True)
 def filter_and_forward(message):
-    if message.text:
-        text = message.text.lower()
 
-        for pattern in FORBIDDEN_PATTERNS:
-            if re.search(pattern, text):
-                print("Заборонене слово знайдено")
-                return
+    if not message.text:
+        return
 
-        bot.forward_message(CHANNEL_ID, message.chat.id, message.message_id)
+    text = message.text.lower()
+
+    # Перевірка на заборонені слова
+    for pattern in FORBIDDEN_PATTERNS:
+        if re.search(pattern, text):
+            print("Повідомлення заблоковано фільтром")
+            return
+
+    # Якщо все чисто — пересилаємо в усі канали
+    for channel_id in CHANNELS:
+        bot.forward_message(channel_id, message.chat.id, message.message_id)
 
 bot.infinity_polling()
